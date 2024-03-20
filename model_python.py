45892723fd486193ccc33c2469a212c9d712d8cd
HEMANTHBS2001 <142691087+HEMANTHBS2001@users.noreply.github.com>
2024-03-20 11:44:23 +0530

    model_python.py

--- model_python.py
+++ model_python.py
@@ -1,11 +1,18 @@
+import streamlit as st
+import pickle
+import cv2
+import numpy as np
+model = pickle.load(open('model.pkcls', 'rb'))
+user_input = st.text_input("ENTER FILE PATH")
 
-import Orange
-import pandas as pd
 
-try:
-    mc = pd.read_pickle("model.pkcls")
-except FileNotFoundError:
-    st.error("File not found. Please check the file path.")
-except Exception as e:
-    st.error(f"Error loading pickle file: {e}")
+
+image = cv2.imread(user_input)
+
+# # Convert BGR image to RGB (if needed)
+# image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+# # Convert the image to a NumPy array
+# numpy_array = np.array(image_rgb)
+prediction = model.predict(image)
 
