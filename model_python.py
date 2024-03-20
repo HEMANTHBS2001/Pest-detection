81a9104f19f03f40dab1951c97616ae2d0cadfb9
HEMANTHBS2001 <142691087+HEMANTHBS2001@users.noreply.github.com>
2024-03-20 12:06:08 +0530

    model_python.py

--- model_python.py
+++ model_python.py
@@ -2,14 +2,14 @@
 
 import streamlit as st
 import pickle
-import cv2
+# import cv2
 import numpy as np
 model = pickle.load(open('model.pkcls', 'rb'))
 user_input = st.text_input("ENTER FILE PATH")
 
 
-
-image = cv2.imread(user_input)
+# model.predict(
+# image = cv2.imread(user_input)
 
 # # Convert BGR image to RGB (if needed)
 # image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
