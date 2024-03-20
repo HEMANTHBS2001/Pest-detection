335c6cb406a7e646ea8cfbbbfa28b67fbcff0ba6
HEMANTHBS2001 <142691087+HEMANTHBS2001@users.noreply.github.com>
2024-03-20 12:21:19 +0530

    model_python.py

--- model_python.py
+++ model_python.py
@@ -3,22 +3,6 @@
 import streamlit as st
 import pickle
 
-st.write('hello')
-# # import cv2
-# import numpy as np
-# with open('model.pkcls', 'rb') as model_file:
-#     model = pickle.load(model_file)
+st.write('PEST DETECTION')
 
-# user_input = st.text_input("ENTER FILE PATH")
-
-
-# model.predict(
-# image = cv2.imread(user_input)
-
-# # Convert BGR image to RGB (if needed)
-# image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-# # Convert the image to a NumPy array
-# numpy_array = np.array(image_rgb)
-prediction = model.predict(image)
 
