ffc4034f25300d917122f1cab92aed9e607aacec
HEMANTHBS2001 <142691087+HEMANTHBS2001@users.noreply.github.com>
2024-03-20 12:32:29 +0530

    model_python.py

--- model_python.py
+++ model_python.py
@@ -1,14 +1,14 @@
 # pip install opencv-python
 
 import streamlit as st
-import Orange
+# import Orange
 import pickle
 import pandas as pd
 import numpy as np
 
 
 
-# st.write('PEST DETECTION')
+st.write('PEST DETECTION')
 # with open("model.pkcls","rb") as model:
 #     loaded_model = pickle.load(model)
 
