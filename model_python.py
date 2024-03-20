ab98f5eadfff5872743a23337cfcda039d6cd94d
HEMANTHBS2001 <142691087+HEMANTHBS2001@users.noreply.github.com>
2024-03-20 12:09:00 +0530

    model_python.py

--- model_python.py
+++ model_python.py
@@ -4,7 +4,9 @@ import streamlit as st
 import pickle
 # import cv2
 import numpy as np
-model = pickle.load(open('model.pkcls', 'rb'))
+with open(model.pkcls, 'rb') as model_file:
+    model = pickle.load(model_file)
+
 user_input = st.text_input("ENTER FILE PATH")
 
 
