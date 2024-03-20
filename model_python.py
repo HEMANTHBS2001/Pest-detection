21f17848d42216464dee89955c7478c176493560
HEMANTHBS2001 <142691087+HEMANTHBS2001@users.noreply.github.com>
2024-03-20 10:33:38 +0530

    model_python.py

--- model_python.py
+++ model_python.py
@@ -1,5 +1,10 @@
-pip install pandas
+
 import pandas as pd
 
-# Read the pickle file
-mc = pd.read_pickle("model.pkcls")
+try:
+    mc = pd.read_pickle("model.pkcls")
+except FileNotFoundError:
+    st.error("File not found. Please check the file path.")
+except Exception as e:
+    st.error(f"Error loading pickle file: {e}")
+
